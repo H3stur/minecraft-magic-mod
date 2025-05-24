b65f1de02620e571106f3fc06da5c51c999eb67b
Taran Roberts <59487009+H3stur@users.noreply.github.com>
2025-05-25 00:24:36 +0100

    More work on the Rune Pedestal
    The rune pedestal now has an inventory slot and can be right clicked

--- MDK-1.21-NeoGradle-main/src/main/java/com/hastur/examplemod/block/RunePedestalBlock.java
+++ MDK-1.21-NeoGradle-main/src/main/java/com/hastur/examplemod/block/RunePedestalBlock.java
@@ -3,7 +3,9 @@ package com.hastur.examplemod.block;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
@@ -18,6 +20,7 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
@@ -63,5 +66,15 @@ public class RunePedestalBlock extends BaseEntityBlock {
         return this.defaultBlockState().setValue(BlockStateProperties.FACING, context.getHorizontalDirection().getOpposite());
     }
 
+    @Override
+    protected InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos,
+                                          Player player, InteractionHand hand, BlockHitResult hitResult){
 
+
+        //if(RunePedestalBlockEntity.inventory.getStackInSlot(0).isEmpty() && !stack.isEmpty()){
+        //
+        //}
+
+        return InteractionResult.SUCCESS;
+    }
 }
--- MDK-1.21-NeoGradle-main/src/main/java/com/hastur/examplemod/block/RunePedestalBlockEntity.java
+++ MDK-1.21-NeoGradle-main/src/main/java/com/hastur/examplemod/block/RunePedestalBlockEntity.java
@@ -1,17 +1,33 @@
 package com.hastur.examplemod.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.ItemStackHandler;
 
 import static com.hastur.examplemod.block.ModBlockEntities.RUNE_PEDESTAL_ENTITY;
 
 public class RunePedestalBlockEntity extends BlockEntity {
-    public RunePedestalBlockEntity(BlockPos pos, BlockState state){
+    public RunePedestalBlockEntity(BlockPos pos, BlockState state) {
         super(RUNE_PEDESTAL_ENTITY.get(), pos, state);
     }
 
+    public final ItemStackHandler inventory = new ItemStackHandler(1) {
+        @Override
+        protected int getStackLimit(int slot, ItemStack stack) {
+            return 1;
+        }
 
+        @Override
+        protected void onContentsChanged(int slot) {
+            setChanged();
+            if (!level.isClientSide()) {
+                level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), 3);
+            }
 
+        }
+
+    };
 
 }
